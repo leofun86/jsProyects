a0acbc55f85ccf08e657648688991e40be83cb63
leofun86 <leorecord@hotmail.com>
2020-07-20 21:42:44 -0300

    carritoDeCompras finalizado && agregado  (sin comenzar)

--- downloaded/2_CarritoDeCompras/js/app.js
+++ downloaded/2_CarritoDeCompras/js/app.js
@@ -5,13 +5,11 @@ const listaCursos = document.querySelector('#lista-carrito tbody');
 const cantidadCursos = document.getElementById('cantidad');
 let cursosAgregados = [];
 const alert = document.getElementById('alert');
-
+const btnVaciarCarrito = document.getElementById('vaciar-carrito');
 
 // Listeners
 document.addEventListener('DOMContentLoaded', () => {
     cargarEventListeners();
-	// Cantidad inicial de cursos
-	cantidad(1);
 });
 
 function cargarEventListeners() {
@@ -20,7 +18,11 @@ function cargarEventListeners() {
 	// Elimina un curso en el carrito
 	carrito.addEventListener('click', eliminarCurso);
 	// Vaciar carrito
-	carrito.addEventListener('click', vaciarCarrito);
+	btnVaciarCarrito.addEventListener('click', vaciarCarrito);
+	// Carga cursos en carrito
+	leerLocalStorage();
+	// Agrega las cantidades de cursos agregados en el carrito en un notificador sobre el ícono de carrito
+	cantidad(1);
 }
 
 // Funciones
@@ -33,6 +35,7 @@ function comprarCurso(e) {
         const curso = e.target.parentElement.parentElement;
         leerDatosCurso(curso);
     };
+	return true;
 }
 
 // Lee los datos del curso agregado
@@ -43,18 +46,32 @@ function leerDatosCurso(curso) {
         precio: curso.querySelector('.precio span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
     };
-	if (cursosAgregados.some(curso => curso === infoCurso.titulo)) {
+	if (localStorage.getItem('cursos') !== null) {
+		if (JSON.parse(localStorage.getItem('cursos')).some(curso => curso.titulo === infoCurso.titulo)) {
+			alert.textContent="El curso ya existe";
+			alert.style.backgroundColor="red";
+			alert.classList.add('alertAnim');
+			setTimeout(()=>{
+				alert.classList.remove('alertAnim');
+			}, 1500);
+		} else {
+			cursosAgregados.push(infoCurso.titulo);
+			insertarCarrito(infoCurso);
+		}
+	} else {
+		if (cursosAgregados.some(curso => curso === infoCurso.titulo)) {
 		alert.textContent="El curso ya existe";
 		alert.style.backgroundColor="red";
 		alert.classList.add('alertAnim');
 		setTimeout(()=>{
 			alert.classList.remove('alertAnim');
 		}, 1500);
-	} else {
-		cursosAgregados.push(infoCurso.titulo);
-		insertarCarrito(infoCurso);
-		console.log(cursosAgregados)
+		} else {
+			cursosAgregados.push(infoCurso.titulo);
+			insertarCarrito(infoCurso);
+		}
 	}
+	return true;
 };
 
 // Muestra el curso seleccionado en el carrito
@@ -69,38 +86,100 @@ function insertarCarrito(curso) {
     `;
     fragment.appendChild(row);
     listaCursos.appendChild(fragment);
+	guardarCursoLocalStorage(curso);
 	cantidad(2);
 	alert.textContent="Curso agregado";
 	alert.style.backgroundColor="green";
 	alert.classList.add('alertAnim');
 	setTimeout(()=>{
 		alert.classList.remove('alertAnim');
-	}, 1500);	
+	}, 1500);
+	return true;
 }
 
 // Quita un curso del carrito
 function eliminarCurso(e) {
-	cursosAgregados = cursosAgregados.filter(curso => curso !== e.target.parentElement.parentElement.querySelector('.titulo').textContent );
 	if (e.target.classList.contains('borrar-curso')) {
+		eliminarCursoLocalStorage(e);
 		e.target.parentElement.parentElement.remove();
-		cantidad(3);
-	};
+	}
+	return true;
 }
 
 // Vaciar el carrito
 function vaciarCarrito(e) {
-	if(e.target.id ==='vaciar-carrito') {
-		carrito.querySelector('tbody').innerHTML="";
-		cant=0;
-		cantidad(1);
+	// Forma lenta
+		//listaCursos.innerHTML='';
+		/*
+		if(e.target.id ==='vaciar-carrito') {
+			carrito.querySelector('tbody').innerHTML="";
+			cant=0;
+			cantidad(1);
+		}
+		*/
+		
+	// Forma rápida
+	while (listaCursos.firstChild) {
+		listaCursos.firstChild.remove();
 	}
+	vaciarLocalStorage();
+	return true;
 }
 
+// Almacena los cursos del carrito a LocalStorage
+function guardarCursoLocalStorage(curso) {
+	let cursos;
+	// Toma datos de LocalStorage o un array vacío
+	cursos = obtenerCursosLocalStorage();
+	console.log(cursos);
+	// El curso seleccionado se agrega al array
+	cursos.push(curso);
+	// Almacena los cursos en LocalStorage
+	localStorage.setItem('cursos', JSON.stringify(cursos));
+	return true;
+}
+
+// Obtiene los cursos almacenados en Local Storage
+function obtenerCursosLocalStorage() {
+	let cursosLS=[];
+	// Comprobamos si hay datos en LocalStorage
+	if (localStorage.getItem('cursos') !== null) {
+		cursosLS = JSON.parse(localStorage.getItem('cursos'));
+	}
+	return cursosLS;
+}
+
+// Imprime los cursos de LocalStorage en el carrito
+function leerLocalStorage() {
+	let LS;
+	cursosLS = obtenerCursosLocalStorage();
+	cursosLS.forEach(curso => {
+		// Construir el template
+		const fragment = document.createDocumentFragment();
+		const row = document.createElement('tr');
+		row.innerHTML = `
+			<td> <img src="${curso.imagen}" width=120 /> </td>
+			<td class="titulo">${curso.titulo}</td>
+			<td>${curso.precio}</td>
+			<td> <a href="#" class="borrar-curso" data-id="${curso.id}">x</a> </td>
+		`;
+		fragment.appendChild(row);
+		listaCursos.appendChild(fragment);
+	});
+	return true;
+};
+
+// Notificación de cantidad de cursos agregados al carrito
 function cantidad(op) {
 	switch (op) {
 		case 1:
-			cant=0;
-			cantidadCursos.textContent=cant;
+			if (localStorage.getItem('cursos') !== null) {
+				cant = JSON.parse(localStorage.getItem('cursos')).length;
+				cantidadCursos.textContent=cant;
+			} else {
+				cant=0;
+				cantidadCursos.textContent=cant;
+			}
 		break;
 		case 2:
 			cant++;	
@@ -111,4 +190,26 @@ function cantidad(op) {
 			cantidadCursos.textContent=cant;
 		break;
 	}
+	return true;
+}
+
+// Elimina el curso en LocalStorage
+function eliminarCursoLocalStorage(e) {
+	if (localStorage.getItem('cursos') !== null) {
+		let cursos = JSON.parse(localStorage.getItem('cursos'));
+		cursos = cursos.filter(curso => curso.id !== e.target.attributes[2].value);
+		localStorage.setItem('cursos', JSON.stringify(cursos));
+		cantidad(1);11
+	} else {
+		cantidad(3);
+	}
+	return true;
+}
+
+// Vacía el array de LocalStorate
+function vaciarLocalStorage() {
+	localStorage.clear();
+	// Alternativa --> localStorage.setItem('cursos', JSON.stringify([]));
+	cantidad(1);
+	return true;
 }
